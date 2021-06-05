00ab870a909223a1742b61d12c163f530d6b865b
HyeonseopJeong <gustjq1101@naver.com>
2021-06-05 20:17:45 +0900

    add some routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,6 +3,7 @@
 use Simple\Routing\Route;
 use App\Middlewares\AuthMiddleware;
 
+// get
 Route::add('get', '/', '\App\Controllers\IndexController::workout', [AuthMiddleware::class]);
 Route::add('get', '/workout', '\App\Controllers\IndexController::workout', [AuthMiddleware::class]);
 Route::add('get', '/record', '\App\Controllers\IndexController::record', [AuthMiddleware::class]);
@@ -14,4 +15,10 @@ Route::add('get', '/workout/{routine_id}/{movement_id}', '\App\Controllers\Worko
 
 Route::add('get', '/google/login', '\App\Controllers\AuthController::login');
 Route::add('get', '/google/logout', '\App\Controllers\AuthController::logout');
+
+
+// post
+Route::add('post', '/setting', '\App\Controllers\IndexController::changeSetting', [AuthMiddleware::class]);
+Route::add('post', '/workout/{routine_id}/{movement_id}', '\App\Controllers\WorkoutController::recordMovement', [AuthMiddleware::class]);
+Route::add('post', '/contact', '\App\Controllers\IndexController::postContact', [AuthMiddleware::class]);
 
